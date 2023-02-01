fb971c276ef34cdb9d51d4243302ed95f6011ed4
Danielle Souza <dany.sa1@hotmail.com>
2023-02-01 17:53:14 -0300

    feat: create service axios api

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import blogFetch from '../axios/config';
+
+import './Home.css';
 
 const Home = () => {
+
+  const [ posts, setPosts]= useState([]);
+
+  const getPosts = async() => {
+    try {
+      const response = await blogFetch.get('/posts')
+      const data = response.data
+      setPosts(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    getPosts();
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>Últimos Posts</h1>
+      {posts.length === 0 ? ( <p>Carregando...</p>) : (
+        posts.map((post) => (
+          <div className='post' key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className='btn'>Ler mais</Link>
+          </div>))
+        )}
+    </div>
   )
 }
 
